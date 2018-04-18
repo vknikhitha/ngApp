05e6fa03d9358eff77003126b9c00fa91035c978
vknikhitha <vknikhitha@gmail.com>
2018-04-18 09:13:14 -0700

    additional fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './courses.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
